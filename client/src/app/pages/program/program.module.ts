ebeff92d09c61bc7e9b5b03b69bcb2058d8d3852
Elwin Schmitz <eschmitz@redcross.nl>
2021-09-01 17:18:14 +0200

    Use SharedModule for generic components

--- client/src/app/pages/program/program.module.ts
+++ client/src/app/pages/program/program.module.ts
@@ -2,12 +2,18 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
-import { HeaderComponent } from 'src/app/components/header/header.component';
+import { SharedModule } from 'src/app/components/shared.module';
 import { ProgramPageRoutingModule } from './program-routing.module';
 import { ProgramPage } from './program.page';
 
 @NgModule({
-  imports: [CommonModule, FormsModule, IonicModule, ProgramPageRoutingModule],
-  declarations: [ProgramPage, HeaderComponent],
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    ProgramPageRoutingModule,
+    SharedModule,
+  ],
+  declarations: [ProgramPage],
 })
 export class ProgramPageModule {}
