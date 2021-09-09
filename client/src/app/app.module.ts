0f15d40240d2d5ab362c80ebf04e390d8f450c1f
Elwin Schmitz <eschmitz@redcross.nl>
2021-09-09 11:42:08 +0200

    Tweak global Ionic-config to reduce data use?

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -18,7 +18,10 @@ export const createTranslateLoader = (http: HttpClient) =>
   entryComponents: [],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(),
+    IonicModule.forRoot({
+      mode: 'md',
+      animated: false,
+    }),
     AppRoutingModule,
     HttpClientModule,
     TranslateModule.forRoot({
