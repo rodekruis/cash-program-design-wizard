081479ca8905d0c136a3c6140b0f9fe769e2d6af
arsforza <asforza@redcross.nl>
2021-09-10 14:48:05 +0200

    #55 Add report page

---
+++ client/src/app/pages/report/report-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ReportPage } from './report.page';
+
+const routes: Routes = [
+  {
+    path: ':id',
+    component: ReportPage,
+  },
+  {
+    path: '**',
+    component: ReportPage,
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class ReportPageRoutingModule {}
---
+++ client/src/app/pages/report/report.page.spec.ts
@@ -0,0 +1,24 @@
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { IonicModule } from '@ionic/angular';
+
+import { ReportPage } from './report.page';
+
+describe('ReportPage', () => {
+  let component: ReportPage;
+  let fixture: ComponentFixture<ReportPage>;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ReportPage ],
+      imports: [IonicModule.forRoot()]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(ReportPage);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
