778af09e74dc39850df20ab5364fb74e7a91b7c9
arsforza <asforza@redcross.nl>
2021-09-10 15:03:23 +0200

    Fix page spec test

--- client/src/app/pages/report/report.page.spec.ts
+++ client/src/app/pages/report/report.page.spec.ts
@@ -1,16 +1,42 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { ActivatedRoute } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
+import { TranslateModule } from '@ngx-translate/core';
+import { of } from 'rxjs';
+import { SharedModule } from 'src/app/components/shared.module';
+import { Role } from 'src/app/models/role.enum';
+import { Tag } from 'src/app/models/tag.enum';
 import { ReportPage } from './report.page';
 
 describe('ReportPage', () => {
   let component: ReportPage;
   let fixture: ComponentFixture<ReportPage>;
 
+  const mockParams = {
+    role: Role.HQ,
+    tag: Tag.data,
+  };
+
   beforeEach(
     waitForAsync(() => {
       TestBed.configureTestingModule({
         declarations: [ReportPage],
-        imports: [IonicModule.forRoot()],
+        imports: [
+          IonicModule.forRoot(),
+          TranslateModule.forRoot(),
+          SharedModule,
+        ],
+        providers: [
+          {
+            provide: ActivatedRoute,
+            useValue: {
+              snapshot: {
+                params: { id: 1 },
+              },
+              queryParams: of(mockParams),
+            },
+          },
+        ],
       }).compileComponents();
 
       fixture = TestBed.createComponent(ReportPage);
