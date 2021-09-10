cd303dfdcd50014f7ceb890168e6d5155711f792
arsforza <asforza@redcross.nl>
2021-09-10 14:51:01 +0200

    Fix lint problems

--- client/src/app/pages/report/report.page.spec.ts
+++ client/src/app/pages/report/report.page.spec.ts
@@ -1,22 +1,23 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
-
 import { ReportPage } from './report.page';
 
 describe('ReportPage', () => {
   let component: ReportPage;
   let fixture: ComponentFixture<ReportPage>;
 
-  beforeEach(waitForAsync(() => {
-    TestBed.configureTestingModule({
-      declarations: [ ReportPage ],
-      imports: [IonicModule.forRoot()]
-    }).compileComponents();
+  beforeEach(
+    waitForAsync(() => {
+      TestBed.configureTestingModule({
+        declarations: [ReportPage],
+        imports: [IonicModule.forRoot()],
+      }).compileComponents();
 
-    fixture = TestBed.createComponent(ReportPage);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  }));
+      fixture = TestBed.createComponent(ReportPage);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+    })
+  );
 
   it('should create', () => {
     expect(component).toBeTruthy();
