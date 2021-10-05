41e8f8080737a5e24417ea240c35e7c09de7d38f
Elwin Schmitz <eschmitz@redcross.nl>
2021-10-05 11:53:34 +0200

    fix: Remove fake-login

--- client/src/app/home/home.page.ts
+++ client/src/app/home/home.page.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { ApiPath, ApiService } from '../services/api.service';
-import { AuthService } from '../services/auth.service';
-import { JwtService } from '../services/jwt.service';
 import { StateService } from '../services/state.service';
 import { TranslatableStringService } from '../services/translatable-string.service';
 
@@ -17,8 +15,6 @@ export class HomePage {
     private apiService: ApiService,
     private translatableString: TranslatableStringService,
     public translate: TranslateService,
-    private jwtService: JwtService,
-    private authService: AuthService,
   ) {}
 
   public changeLanguage(event: any) {
@@ -31,22 +27,5 @@ export class HomePage {
     this.apiService.get(ApiPath.test).subscribe((response) => {
       window.alert(this.translatableString.get(response));
     });
-  }
-
-  public apiFakeLogIn() {
-    const fakeToken =
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.' +
-      'eyJpZCI6MiwidXNlcm5hbWUiOiJ1c2VyMkBleGFtcGxlLm9yZyIsImV4cCI6MTYzODE4OTA1NC43MjgsImlhdCI6MTYzMzAwMTQ1NH0.' +
-      'WnZV5VdrU3MMh_PW2W0YM5nLbC-DaGa7E4sKsWXIZSg';
-    const input = window.prompt('Token?', fakeToken);
-
-    if (!input) {
-      return;
-    }
-    this.jwtService.saveToken(input);
-  }
-
-  public apiFakeLogOut() {
-    this.authService.logout();
   }
 }
