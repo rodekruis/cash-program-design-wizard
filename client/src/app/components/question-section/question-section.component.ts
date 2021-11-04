4566720d1dfdf4b468d93b02220c74ee9e266649
Elwin Schmitz <eschmitz@redcross.nl>
2021-11-04 14:41:07 +0100

    Hide questions based on Tag-filter

--- client/src/app/components/question-section/question-section.component.ts
+++ client/src/app/components/question-section/question-section.component.ts
@@ -1,9 +1,13 @@
 import { Component, Input } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { Tag } from 'src/app/models/tag.enum';
 import { ProgramDataService } from 'src/app/services/program-data.service';
 import { StateService } from 'src/app/services/state.service';
 import { QuestionInput, QuestionType } from 'src/app/types/question-input.type';
-import { QuestionSection } from 'src/app/types/question-section.type';
+import {
+  QuestionSection,
+  QuestionSubsection,
+} from 'src/app/types/question-section.type';
 
 @Component({
   selector: 'app-question-section',
@@ -27,6 +31,27 @@ export class QuestionSectionComponent {
 
   public onChangeAnswer(question: QuestionInput) {
     this.programData.saveAnswer(this.state.programId, question);
+  }
+
+  public shouldShowQuestion(questionTags: QuestionInput['tags']): boolean {
+    return (
+      this.state.filters.tag === Tag.all ||
+      questionTags.includes(this.state.filters.tag)
+    );
+  }
+
+  public isSubSectionEmpty(subsection: QuestionSubsection): boolean {
+    if (
+      !subsection.questions ||
+      (this.state.filters.tag !== Tag.all &&
+        !subsection.questions.some((question) =>
+          question.tags.includes(this.state.filters.tag),
+        ))
+    ) {
+      return true;
+    }
+
+    return false;
   }
 
   public showComments(question: QuestionInput): boolean {
