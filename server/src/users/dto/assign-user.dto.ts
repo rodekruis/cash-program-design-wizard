5a4c81cbdca5b32415706e56bb57c3c275a08e74
Elwin Schmitz <eschmitz@redcross.nl>
2021-11-10 01:09:46 +0100

    fix: Remove useless validation-conditions + update example(s)

--- server/src/users/dto/assign-user.dto.ts
+++ server/src/users/dto/assign-user.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 import { UserRoleEnum } from './../enum/user-role.enum';
 
 export class AssignUserDto {
@@ -9,14 +9,13 @@ export class AssignUserDto {
   public readonly userName: string;
 
   @ApiProperty({
-    example: Object.values(UserRoleEnum),
+    example: Object.values(UserRoleEnum).join(' | '),
     enum: UserRoleEnum,
   })
   @IsNotEmpty()
-  @MinLength(8)
   public readonly role: UserRoleEnum;
 
-  @ApiProperty({ example: 1 })
+  @ApiProperty({ example: 'b47920c1-b24c-4021-906d-3314a5177d44' })
   @IsString()
   public readonly programId: string;
 }
