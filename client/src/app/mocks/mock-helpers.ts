c61fc35a2a801c26346add3447ef1f51840d8e7c
Elwin Schmitz <eschmitz@redcross.nl>
2021-11-22 11:34:25 +0100

    Reduce Program mock-data for testing

--- client/src/app/mocks/mock-helpers.ts
+++ client/src/app/mocks/mock-helpers.ts
@@ -4,18 +4,25 @@ import { QuestionComment } from '../types/question-input.type';
 export const getRandomNumber = (min: number, max: number): number =>
   Math.round(Math.random() * (max - min) + min);
 
-export const getRandomComment = (): QuestionComment | null =>
-  Math.random() >= 0.5
-    ? {
-        id: getMockId(),
-        text: `Lorem ipsum...`,
-        userName: 'test-user',
-        created: new Date().toISOString(),
-      }
-    : null;
+export const getRandomComment = (): QuestionComment | null => {
+  if (Math.random() >= 0.5) {
+    return {
+      id: getMockId(),
+      text: getRandomFrom([
+        'Lorem ipsum...',
+        'Dolor sit amet.',
+        'consectetur adipiscing elit.',
+      ]),
+      userName: getRandomFrom(['test-user', 'another user']),
+      created: new Date().toISOString(),
+    };
+  }
+};
 
-export const getRandomTag = (): Tag =>
-  Object.values(Tag)[getRandomNumber(0, Object.values(Tag).length - 1)];
+export const getRandomTag = (): Tag => getRandomFrom(Object.values(Tag));
+
+export const getRandomFrom = (fromArray: any[]) =>
+  fromArray[getRandomNumber(0, fromArray.length - 1)];
 
 export const getMockId = (): string =>
   `00000000-0000-0000-0000-${Math.random().toString().substr(2, 12)}`;
