83d3c1176520baa15a2e0b84bcb6bb0dfd14f416
arsforza <asforza@redcross.nl>
2021-11-29 15:16:00 +0100

    Create toaster

---
+++ client/src/app/services/offline.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { ToastController } from '@ionic/angular';
+import { TranslateService } from '@ngx-translate/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class OfflineService {
+  constructor(
+    public toastController: ToastController,
+    private translate: TranslateService,
+  ) {}
+
+  async presentToast() {
+    const toast = await this.toastController.create({
+      position: 'top',
+      cssClass: 'offline-toaster',
+      color: 'secondary',
+      message: this.translate.instant(
+        'connection-status.offline.toaster-message',
+      ),
+      duration: 2000,
+    });
+    toast.present();
+  }
+}
