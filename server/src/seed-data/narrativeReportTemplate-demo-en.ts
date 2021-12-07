471ce533fc820ccc348e7a5e0d5c3b13ea83d8f2
ORCANLRC <34653586+ORCANLRC@users.noreply.github.com>
2021-12-07 15:58:31 +0100

    Update narrativeReportTemplate-demo-en.ts

--- server/src/seed-data/narrativeReportTemplate-demo-en.ts
+++ server/src/seed-data/narrativeReportTemplate-demo-en.ts
@@ -1,8 +1,20 @@
 export const narrativeReportTemplateDemoEn = `
+
 ## The people we will help are mostly:
 
 {{pa-gender-01}}, aged {{pa-age-01 }}, {{pa-marital-status-01}}, {{pa-shelter-status-01}} with {{pa-dependants-01}}.
 They mostly works as {{pa-job-type-01}} and are mostly in need of {{pa-support-areas-01}}.
 
 This due to {{pa-disaster-type-01}}. This program will/is happen/happening in the {{pa-current-disaster-phase-01}}.
+
+### Digtial use by AW & PA 
+
+The majority of the PA ha {{digital-devices-01}}
+
+
+
+
+
+
+
 `;
