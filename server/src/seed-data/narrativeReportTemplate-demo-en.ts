11689626888425638ee9b6e4de4f9d9638fe9759
ORCANLRC <34653586+ORCANLRC@users.noreply.github.com>
2021-12-07 15:55:54 +0100

    Update narrativeReportTemplate-demo-en.ts

--- server/src/seed-data/narrativeReportTemplate-demo-en.ts
+++ server/src/seed-data/narrativeReportTemplate-demo-en.ts
@@ -1,8 +1,48 @@
 export const narrativeReportTemplateDemoEn = `
+
+
 ## The people we will help are mostly:
 
 {{pa-gender-01}}, aged {{pa-age-01 }}, {{pa-marital-status-01}}, {{pa-shelter-status-01}} with {{pa-dependants-01}}.
 They mostly works as {{pa-job-type-01}} and are mostly in need of {{pa-support-areas-01}}.
 
 This due to {{pa-disaster-type-01}}. This program will/is happen/happening in the {{pa-current-disaster-phase-01}}.
+
+### MARKET ACCESS
+
+Things to take into conisderation 
+
+Which stores to consider 
+{{voucher}}
+
+
+
+### REGISTRATION 
+
+The people who will be targetd to register will most likely be using their own devices and have mostly access to {{digital-devices-01}}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 `;
