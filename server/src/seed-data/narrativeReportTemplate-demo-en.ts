1f6d180ca1d4c8d3d177913012620a0c61895efd
Elwin Schmitz <eschmitz@redcross.nl>
2021-12-08 09:22:25 +0100

    Update narrativeReportTemplate-demo-en.ts

--- server/src/seed-data/narrativeReportTemplate-demo-en.ts
+++ server/src/seed-data/narrativeReportTemplate-demo-en.ts
@@ -1,23 +1,27 @@
 export const narrativeReportTemplateDemoEn = `
-
-
-
 ## PROGRAM DETAILS
 
-PROGRAM LOCATION: {{electricity-frequency-03}}
-
-START DATE: 
-
-END DATE:
-
-BUDGET: 
+- PROGRAM LOCATION: {{program-location}}
+- START DATE: {{program-date-start}}
+- END DATE: {{program-date-end}}
+- BUDGET: {{program-budget}}
 
 ## Situation Assessment
 
 
-The targeted location has been affected by {{pa-disaster-type-01}}. This program will/is happen/happening in the {{pa-current-disaster-phase-01}}phase.
+The targeted location has been affected by:
+{{pa-disaster-type-01}}
 
-The targeted people affected are mostly {{pa-gender-01}}, aged {{pa-age-01 }}, {{pa-marital-status-01}}, {{pa-shelter-status-01}} with {{pa-dependants-01}}.
+This program will/is happen/happening in the phase:
+{{pa-current-disaster-phase-01}}
+
+The targeted people affected are mostly:
+{{pa-gender-01}}
+aged {{pa-age-01}},
+{{pa-marital-status-01}},
+{{pa-shelter-status-01}}
+ with {{pa-dependants-01}}
+
 They mostly work as {{pa-job-type-01}} and are mostly in need of {{pa-support-areas-01}}.
 The total amount of people/households affected is curerntly estimated at {{budget-all-02}}
 
@@ -30,7 +34,7 @@ The total amount of people/households affected is curerntly estimated at {{budge
 The donor currency is {{currencies-03}} and the prefered local curency for districution is {{budget-01}}. Total budget asked {{budget-all-05}} in {{budget-01}}. As mentioned the total amount of people/households affected is {{budget-all-02}} however we will be specifically targeting {{budget-all-03}}
 
 
-## Stakeholders included and brought into the discussion are:
+### Stakeholders included and brought into the discussion are:
 
 {{pa-support-areas-03}}
 {{pa-job-type-03}}
@@ -59,24 +63,5 @@ Other info depending
 ### Title
 
 Other info depending
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 `;
