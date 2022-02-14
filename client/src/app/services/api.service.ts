6335c8c96306b5a0b4fae8666dbc9881b0af4600
Elwin Schmitz <eschmitz@redcross.nl>
2022-02-14 16:34:26 +0100

    feat: Add file-upload support to ApiService

--- client/src/app/services/api.service.ts
+++ client/src/app/services/api.service.ts
@@ -13,6 +13,7 @@ export enum ApiPath {
   answers = 'answers',
   comments = 'comments',
   scriptsExport = 'scripts/export',
+  scriptsImport = 'scripts/import',
 }
 
 @Injectable({
@@ -45,10 +46,15 @@ export class ApiService {
       );
   }
 
-  public post(path: ApiPath, body: any, _params?: HttpParams): Observable<any> {
+  public post(
+    path: ApiPath,
+    body: any,
+    _params?: HttpParams,
+    isUpload?: boolean,
+  ): Observable<any> {
     return this.http
       .post(`${environment.apiUrl}/${path}`, body, {
-        headers: this.createHeaders(),
+        headers: this.createHeaders(isUpload),
       })
       .pipe(
         tap((response) => {
@@ -65,13 +71,18 @@ export class ApiService {
       );
   }
 
-  private createHeaders(): HttpHeaders {
-    const headers = new HttpHeaders({
+  private createHeaders(isUpload = false): HttpHeaders {
+    let headers = new HttpHeaders({
       // eslint-disable-next-line @typescript-eslint/naming-convention
       Accept: 'application/json',
       // eslint-disable-next-line @typescript-eslint/naming-convention
       'Content-Type': 'application/json',
     });
+
+    if (isUpload) {
+      headers = headers.delete('Content-Type');
+    }
+
     const token = this.jwtService.getToken();
 
     return headers.set('Authorization', `Token ${token}`);
